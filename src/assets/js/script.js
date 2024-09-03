f5c1b7a4d3f64a43b9d276abecffbff5480175bd
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2024-09-03 13:22:10 -0400

    update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,17 +1,25 @@
-# Swaks is an smtp of CURL, install it first:
-  curl http://www.jetmore.org/john/code/swaks/files/swaks-20130209.0/swaks -o swaks
-  
-  # Set the permissions for the script so you can run it
-  chmod +x swaks
-  
-  # It's based on perl, so install perl
-  sudo apt-get -y install perl
-  
-  # Now send!
-  ./swaks --auth \
-  	--server smtp.mailgun.org \
-  	--au postmaster@whitbystudioswebdesign.com \
-  	--ap 3kh9umujora5 \
-  	--to bar@example.com \
-  	--h-Subject: "Hello" \
-  	--body 'Testing some Mailgun awesomness!'
+// script.js
+document
+  .getElementById("cs-form-1150")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const email = document.getElementById("email-1150").value;
+    const message = document.getElementById("message-1150").value;
+
+    Email.send({
+      Host: "smtp.mailgun.org",
+      Username: "postmaster@whitbystudioswebdesign.com",
+      Password: "c61516184a8f52403d7f56f7f0add324-777a617d-05cd71bf",
+      To: "recipient@example.com", // Change this to the recipient's email
+      From: email,
+      Subject: "New Message from Contact Form",
+      Body: message,
+    })
+      .then(function (message) {
+        alert("Email sent successfully!");
+      })
+      .catch(function (error) {
+        alert("Failed to send email: " + error);
+      });
+  });
